84211e76f59a2ca28aa4e2c7884c4386f2b49018
Hitesh Chalise <hiteshchalise10@gmail.com>
2022-04-26 17:42:30 +0545

    added custom validation for number

--- models/phonebook.js
+++ models/phonebook.js
@@ -13,7 +13,14 @@ const phonebookSchema = new mongoose.Schema({
     },
     number: {
         type: String,
-        required: true
+        minlength: 8,
+        validate: {
+            validator: function (v) {
+                return /(^\d{2}|\d{3})-(\d+$)/gm.test(v);
+            },
+            message: props => `${props.value} is not a valid phone number!`
+        },
+        required: [true, 'Phone number required']
     }
 });
 
